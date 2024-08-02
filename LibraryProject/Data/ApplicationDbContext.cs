d9e37e2ef7e11d514fb1a302d47e4da4e5b0e98b
Łukasz Legenc <lukasz.legenc@wp.pl>
2024-08-02 19:03:43 +0200

    Added seeding core roles

--- LibraryProject/Data/ApplicationDbContext.cs
+++ LibraryProject/Data/ApplicationDbContext.cs
@@ -27,33 +27,63 @@ namespace LibraryProject.Data
             var ADMIN_ROLE_ID = Guid.NewGuid().ToString();
             var ADMIN_USER_ID = Guid.NewGuid().ToString();
 
-            var ADMIN_ROLE = new IdentityRole
+            var EMPLOYEE_ROLE_ID = Guid.NewGuid().ToString();
+            var EMPLOYEE_USER_ID = Guid.NewGuid().ToString();
+
+            var ROLES = new List<IdentityRole>
             {
-                Name = "Admin",
-                NormalizedName = "ADMIN",
-                Id = ADMIN_ROLE_ID,
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    Id = ADMIN_ROLE_ID,
+                },
+
+                new IdentityRole
+                {
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE",
+                    Id = EMPLOYEE_ROLE_ID,
+                }
             };
 
-            var ADMIN_USER = new IdentityUser
+            var CORE_USERS = new List<IdentityUser>
             {
-                Id = ADMIN_USER_ID.ToString(),
-                UserName = "admin@websiteadmin.com",
-                NormalizedUserName = "ADMIN@WEBSITEADMIN.COM",
-                Email = "admin@websiteadmin.com",
-                EmailConfirmed = true,
-                SecurityStamp = string.Empty,
+                new IdentityUser
+                {
+                    Id = ADMIN_USER_ID.ToString(),
+                    UserName = "admin@websiteadmin.com",
+                    NormalizedUserName = "ADMIN@WEBSITEADMIN.COM",
+                    Email = "admin@websiteadmin.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = string.Empty,
+                    PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "admin123"),
+                },
+                new IdentityUser
+                {
+                    Id = EMPLOYEE_USER_ID.ToString(),
+                    UserName = "employee@websiteempl.com",
+                    NormalizedUserName = "EMPLOYEE@WEBSITEEMPL.COM",
+                    Email = "employee@websiteempl.com",
+                    EmailConfirmed = true,
+                    SecurityStamp = string.Empty,
+                    PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "empl123"),
+                }
             };
-
-            var hasher = new PasswordHasher<IdentityUser>();
-            ADMIN_USER.PasswordHash = hasher.HashPassword(ADMIN_USER, "admin123");
-
-            builder.Entity<IdentityRole>().HasData(ADMIN_ROLE);
-            builder.Entity<IdentityUser>().HasData(ADMIN_USER);
+     
+            builder.Entity<IdentityRole>().HasData(ROLES);
+            builder.Entity<IdentityUser>().HasData(CORE_USERS);
 
             builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
             {
                 RoleId = ADMIN_ROLE_ID,
                 UserId = ADMIN_USER_ID,
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = EMPLOYEE_ROLE_ID,
+                UserId = EMPLOYEE_USER_ID,
             });
         }
     }
